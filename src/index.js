3c0d707553f70e881dc5dde9f6591fb715288cc6
Bobban <Bobban@DESKTOP-57L0M99>
2025-02-26 21:37:43 +0100

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';  // Import Router
-import { StrictMode } from 'react';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
-  <StrictMode>
-    <Router basename="/">  {/* Set the base path for your GitHub Pages repo */}
-      <App />
-    </Router>
-  </StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
